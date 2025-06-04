751267c917a51eb70fa7b7b901e4ca2fb7ac8cc6
chvg1968 <conradovilla@gmail.com>
2025-06-04 08:21:38 -0500

    fix: update Ocean Serenity villa wifi password from LasPalmas to LasBrisas

--- properties.js
+++ properties.js
@@ -341,7 +341,7 @@ export const villas = {
       "qr_code": "/assets/images/properties/oceanserenity/Ocean_Serenity-qrcode.png",
       "qr_code_resort": "/assets/images/wifi_qr_resort.png",
       "villa_network": "SeaSalt-101",
-      "villa_password": "LasPalmas",
+      "villa_password": "LasBrisas",
       "resort_network": "Bahia Beach Resort",
       "resort_password": "welcomeHome<br>Note: The H in Home is capitalized",
     },
